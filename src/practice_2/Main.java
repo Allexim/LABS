930349908048ae1d7896ab49c9dc30fcb1fe342c
Allexim <drupal@mamatuik.com>
2022-04-17 21:03:42 +0300

    Ex.#2 with the FOR cycle

--- src/practice_2/Main.java
+++ src/practice_2/Main.java
@@ -6,33 +6,32 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        // write your code here
- //       String output_string = Input_from_keyboard.keyboard_input(); //output of the buffered element as STRING
-//        System.out.println("I am the buffered string from keyboard as chars " + output_string);
+    public static void main(String[] args) {
 
-//       String_to_Integer string_to_integer = new String_to_Integer();
-        // String output_string = Input_from_keyboard.keyboard_input();
+        System.out.println("Guess the number (with cycles)\n");
+
+        //       input();
 
         int comp_number = random();
 
-        int buffered_int = String_to_Integer.conversion_to_integer(); //returns integer from the keyboard input
-        // !!!without exception to non-numeric chars
-        // System.out.println("\nYou have entered " + buffered_int);
+        System.out.println("You have 3 tries to guess the number\n");
 
 
+        for (int i = 0; i < 3; i++) {
 
+            int humNumber = scan();
 
+            if (humNumber == comp_number) {
+                System.out.println("You win");
 
-        if (buffered_int == comp_number) {
-            System.out.println("You win");
+            } else if (humNumber < comp_number) {
+                System.out.println("You have lost");
+
+            } else {
+                System.out.println("Draw");
+
+            }
         }
-        else if (buffered_int < comp_number) {
-            System.out.println("You have lost");
-        }
-        else {
-            System.out.println("Draw");
-        }
 
 
 
@@ -42,19 +41,25 @@ public class Main {
 
 
 
-//        System.out.println("I am integer from keyboard " + string_to_integer.conversion_to_integer());
-
-        // int converted_to_int = String_to_Integer.conversion_to_integer();
 
     }
 
-    public static int random() {
+    public static int random () {
         Random generated_number = new Random();
         int integer_number = generated_number.nextInt(9) + 1;
-        System.out.println("Computer-generated # is " + integer_number);
+        System.out.println("Check for computer-generated number: " + integer_number);
         return integer_number;
 
     }
+
+    public static int scan () {
+        System.out.print("Input number from 1 to 10: ");
+        Scanner scan_number = new Scanner(System.in);
+        int human_number = scan_number.nextInt();
+        return human_number;
+
+    }
+
 
 
 }
