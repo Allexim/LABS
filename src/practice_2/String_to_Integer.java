128f87858e57c5f08a1a29d379a029ac1206ef5c
Allexim <drupal@mamatuik.com>
2022-04-18 18:05:28 +0300

    Ex.#2 correct with BuffReader

--- src/practice_2/String_to_Integer.java
+++ src/practice_2/String_to_Integer.java
@@ -5,19 +5,20 @@ public class String_to_Integer extends Input_from_keyboard {
     private  int output_int;
 
     public String_to_Integer(String str_a) {
+
         super(str_a);
     }
     public static int conversion_to_integer() throws Exception {
         String output_string = Input_from_keyboard.keyboard_input();
-
+        int number = Integer.parseInt(output_string);
         try{
-            int number = Integer.parseInt(output_string);
+       //     int number = Integer.parseInt(output_string);  - old
             System.out.println("I am the string-to-integer variable " +number);
         }
         catch (NumberFormatException ex){
             ex.printStackTrace();
         }
-        return 0;
+        return number;
 
     }
 
